a00428aa1834403a32a7819ec497d9ef92e2df28
Raphael velasco <raphaelvelasco2@gmail.com>
2022-01-18 22:27:42 -0300

    Fiz o desafio

---
+++ script.js
@@ -0,0 +1,23 @@
+for (let i = 1; i <= 10; i++){
+    console.log(i)
+}
+let accountant = 10
+
+while (1 <= accountant){
+    console.log(accountant)
+    accountant -= 1
+}
+
+let accountant2 = 1
+
+while (100 >= accountant2){
+    console.log(accountant2)
+    accountant2 += 2
+}
+
+let accountant3 = 0
+
+while (100 >= accountant3){
+    console.log(accountant3)
+    accountant3 += 2
+}
